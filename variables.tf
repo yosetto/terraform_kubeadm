c6bda6b3012ef778f35a3b0c572f801fe20d7e2d
Youssef <ettolbayos@gmail.com>
2023-01-07 00:16:14 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+   description = "AWS Region to launch servers"
+   default = "us-east-1"
+}
+
+variable "aws_access_key" {
+   description = "AWS User Access Key"
+   default = "ASIARDEHJGIYOPEY4YFU"
+}
+
+variable "aws_secret_key" {
+   description = "AWS User Secret Key"
+   default = "K/l2YT3n3XlLLg/rOPjOM2XORly1OwBLpk0FfLI3"
+}
+
+variable "aws_api_token" {
+   description = "AWS API Token"
+   default = "FwoGZXIvYXdzEO///////////wEaDM87kDSIH7A/tLKJECK1ASVNMg7XxQDkoSmNJWxNgFnPXuBblml4ezgd3Gk7Hr2nRhgl9HJ0axRhgEkm5wn7jSnHmJrSOFlgFVa7uQKpypZEDx0XDRKlLXwCH7sL6xWICyrSHp/I0nABjIoGcvkj98GD099WkxvVcxQXknm48WrbAZZLoLERIW8IJbd45FEHWtnZaOHjFrierwcYvHLn171V5ALHnwSxgNV2v9zQDeEWKul6ouBxdLQ18Z8+ZMhSq+VReREou6/inQYyLaCKGGbB0tvMVfhjAXQcqVSMMV+1SaCr3zK/LbzGE4ec0RNrCpDZmcqkHddczQ=="
+}
